4afe12fda14a0d5742417372474ac008244fedb2
HiramRV <rub15044@uvg.edu.gt>
2016-08-22 22:36:24 -0600

    Codigo Final

--- SimuladorHDT5.py
+++ SimuladorHDT5.py
@@ -4,9 +4,9 @@
 #Erick Bautista 15192
 #Hoja de trabajo 5
 
-#aqui se importan las librerias necesarias para el programa
-import random
+
 import simpy
+import random
 
 """
 Simulacion de un sistema operativo de tiempo compartido
@@ -15,45 +15,54 @@ Simulacion de un sistema operativo de tiempo compartido
 """
 RANDOM_SEED = 22                #Semilla para el random
 Procesos = 25                   # Numero de procesos
-INTERVAL_Procesos = 10.0        # Tiempo necesario para realizar procesos
 Memoria = 100                   #Capacidad de memoria del procesador
+Min_p = 1			#Minima cantidad de instrucciones
+Max_p = 10			#Maxima cantidad de instrucciones
 
 #Definicion de los procesos
-def new(env,capacidadP):
-    "Se generan los procesos, verificando el espacio en memoria"
-    memoriaN= random.randrange(1,10,1): #Espacio de memoria necesario
-    if capacidadP.level - capacidadP.capacity < 0:
-		#No hay espacio en memoria y necesita esperar
-		print('Proceso en espera en %d' % env.now)
-		yield env.timeout(10)
-		
-	else:
+def function(env, tProceso,codigo,RAM, memoriaNecesaria,cantInstrucciones,IPM):
+    
+	#Se imprime el tiempo de llegada
+        yield env.timeout(tProceso)
+        print('Tiempo: %f - %s solicita %d de memoria ram' % (env.now, codigo, memoriaNecesaria))
+        tiempo_llegada = env.now
 	
-    ready()
-    #Hace cola para esperar un espacio de memoria
+    #Se solicita un espacio de memoria de la RAM
+        yield RAM.get(memoriaNecesaria)
+        print('Tiempo: %f - %s (Solicitud de RAM)%d de RAM,aceptada' % (env.now, codigo, memoriaNecesaria))
+	
+	#Instrucciones completadas
+        cantInstC = 0
+        while cantInstC < cantInstrucciones:
+        #Nos conectamos a la CPU (Estado Ready)
+                with CPU.request() as req:
+                        yield req
+                        #Obtener el numero de instrucciones a realizar
+                        if(cantInstrucciones-cantInstC)>=IPM:
+                                #Mas de 3 instrucciones a realizar
+                                instEfectuar = IPM
 
-            
-def ready():
-    #El proceso esta listo, pero depende de la atencion del CPU
-    contador = random.randrange(1,10,1) #Cantidad de instrucciones a realizar
-    #Cuando el CPU se desocupa
-    running()
-    #De lo contrario espera
-
-def running(contador ):
-    #Atiende un proceso por tiempo, el tiempo suficiente para realizar 3 instruccines
-    instrucciones a realizar = contador
-    #Las instrucciones a realizar se actualizan cada tiempo
-    #Si le quedan solo 3 instrucciones por realizar, lo libera rapidamente
-    #Posibilidades
-
-    #Terminated: Se realizaron todas las instruccioenes y se sale del sistema
-    #Waiting:
-    nA = random.randrange(1,10,1) #Se genera un numero al azar entre 1 y 10
-    #Ai nA es == 1
-    #Pasa a la cola de Waiting y regresa a ready
-    #Ready: Si el numero es mayor que 1, regresa a la cola de ready
-
+                        else:
+                                #Menos de 3 instrucciones a realizar
+                                instEfectuar=(cantInstrucciones-cantInstC)
+			#Imprimimos el tiempo que se tomara en realizar la instruccion
+                        print('Tiempo necesario: %f - %s (ready) cpu %d ' % (env.now, codigo, instEfectuar))
+			 #Se toma la cantidad de recursos necesaria
+                        yield env.timeout(instEfectuar/IPM)   #Error
+			#Actualizamos cantidad de instrucciones realizadas
+                        cantInstC += instEfectuar
+			 
+		#Se genera un random para ver si se atiende el proceso o se pone en espera
+                        validador = random.randint(1,2)
+                        if validador == 1 and instEfectuar<IPM:
+                                with Wait.request() as reqE:
+                                        yield reqE
+				#Espera el tiempo necesario para hacer otra solicitud
+                                yield env.timeout(1)
+	
+	#Se retornan los recursos a la "memmoria"
+        yield RAM.put(memoriaNecesaria)
+        print('Fin proceso %f - %s, Utilizo %d de memoria' % (env.now, codigo, memoriaNecesaria))
 
     
 #Inicio de la simulacion
@@ -62,17 +71,21 @@ print "  #    	  Hoja de trabajo #5         #"
 print "  #              SYMPY               #"
 print "  ####################################"
 print('Simulacion de procesos')
-random.seed(RANDOM_SEED)                #Se crea siempre el mismo random, para poder comparar resultados despues
 
-env = simpy.Environment()                                   #Se crea el ambiente de simulacion
-procesador = simpy.Resource(env, capacity = 1)              #Se podra realizar solo un proceso a la vez
-capacidadP = simpy.Container(env,Procesos,init=Procesos)    #Uso de container
+random.seed(RANDOM_SEED)                	        #Se crea siempre el mismo random, para poder comparar resultados despues
+env = simpy.Environment()                               #Se crea el ambiente de simulacion
+CPU = simpy.Resource(env, capacity = 1)                 #Se podra realizar solo un proceso a la vez
+RAM = simpy.Container(env,init=Memoria,capacity= 100)   #Uso de container, tiene capacidad de 100, y comienza con toda la capacidad
+Wait= simpy.Resource(env, capacity = 1)			#Capacidad de cola  
+instPM = 3.0           #Intrucciones por minuto CPU
+constante = 1
+
+#Creacion de procesos
+for i in range(Procesos):
+	tProceso = random.expovariate(1.0/constante)
+	memoriaNecesaria= random.randint(Min_p,Max_p)  #Memoria a solicitar
+	cantInstrucciones= random.randint(Min_p,Max_p) #Intrucciones que necesitaran 
+	env.process(function(env, tProceso,'Proceso %d' % i,RAM, memoriaNecesaria,cantInstrucciones,instPM))
 
 #Comienza el proceso de ejecucion
-env.process(new(env,capacidadP))        #Proceso 
 env.run()                               #Se corre la simulacion hasta que no existan mas eventos
-
-# env.now() imprime el tiempo de simulacion actual
-#Yield = return
-# random.randrange(1,10,1) random entre 1 y 10
-
